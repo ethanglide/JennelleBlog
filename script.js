4297faec6fbd395a309eb9c9fbf0404cb0bd39ab
ethanglide <92652573+ethanglide@users.noreply.github.com>
2022-05-12 12:51:02 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("Secret Found!");
-let previousPage = '0';
-
-function SetMain(pagenum) {
-  document.getElementById(previousPage).style.display = "none";
-  document.getElementById(pagenum).style.display = "block";
-  previousPage = pagenum;
-}
